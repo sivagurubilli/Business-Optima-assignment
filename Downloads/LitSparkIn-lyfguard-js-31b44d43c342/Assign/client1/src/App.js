bd5a6cfe6bc8031f53b21169a8c13bed409c5b6e
octal-optimum <gurubilli.siva@octaloptimum.com>
2024-09-10 19:56:24 +0530

    initial coommit

---
+++ Downloads/LitSparkIn-lyfguard-js-31b44d43c342/Assign/client1/src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Signup from "./comoponents/Signup";
+import ProtectedRoutes from "./ProtestedRoutes";
+import Login from "./comoponents/Login";
+import Profile from "./comoponents/Profile";
+
+
+
+function App() {
+  return (
+
+    <Routes>
+        <Route path="/login"  element={<Login />} />
+        <Route path="/register" element={<Signup />} />
+      <Route path="/" element={<ProtectedRoutes />}>
+        <Route path="/profile" element={<Profile />} />
+      </Route>
+    </Routes>
+
+
+  );//
+}
+
+export default App;
